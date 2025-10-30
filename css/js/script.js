0a2cd1ef0353872433d8587a32310d0e5485b25b
Kauany Ferreira <kauany.ferrera@icloud.com>
2025-10-30 17:43:41 -0300

    JS script

---
+++ css/js/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.querySelector(".menu").classList.toggle("ativo");
+}
+
+function abrirModal() {
+  document.getElementById("meuModal").style.display = "flex";
+}
+
+function fecharModal() {
+  document.getElementById("meuModal").style.display = "none";
+}
